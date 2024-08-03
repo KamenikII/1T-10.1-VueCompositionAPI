98e84d20c6644e5c6c1a06d12305e6e0857cbd62
Ilya <107436210+KamenikII@users.noreply.github.com>
2024-08-03 21:29:27 +0300

    import firebase, add function to 'done' TODO

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,25 @@
       </div>
     </form>
 
-    <div class="card mb-5" :key="todo.id" v-for="todo in todos">
+    <div 
+      class="card mb-5"
+      :class="{'has-background-success-light': todo.done}"
+      :key="todo.id" 
+      v-for="todo in todos">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">{{todo.content}}</div>
+            <div class="column"
+              :class="{'has-text-success line-through': todo.done}"
+            >{{todo.content}}</div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">&check;</button>
-              <button class="button is-danger ml-2">&cross;</button>
+              <button 
+                @click="togglerDone(todo.id)"
+                class="button is-light"
+                :class="todo.done ? 'is-success' : 'is-light'"> &check; </button>
+              <button 
+                class="button is-danger ml-2"
+                @click="deleteToDo(todo.id)"> &cross; </button>
             </div>
           </div>
         </div>
@@ -37,24 +48,31 @@
 
 <script setup> //setup - для однофайловых компонентов
 // imports
-import { ref } from 'vue'
-import { v4 as uuidv4 } from 'uuid'
+  import { ref } from 'vue'
+  import { v4 as uuidv4 } from 'uuid'
 
 // todos
-const todos = ref([])
+  const todos = ref([])
 
 // methods
-const addToDo =()=>{
-  const newToDo = {
-    id: uuidv4(),
-    content: newToDoContent.value,
-    done: false
+  const addToDo =()=>{
+    const newToDo = {
+      id: uuidv4(),
+      content: newToDoContent.value,
+      done: false
+    }
+    
+    todos.value.unshift(newToDo)
+    newToDoContent.value = ""
   }
-  
-  todos.value.unshift(newToDo)
-  newToDoContent.value = ""
-}
-const newToDoContent = ref("")
+  const newToDoContent = ref("")
+  const deleteToDo = id => {
+    todos.value = todos.value.filter(todo => todo.id !== id)
+  }
+  const togglerDone = id => {
+    const index = todos.value.findIndex(todo => todo.id === id)
+    todos.value[index].done = !todos.value[index].done
+  }
 </script>
 
 
@@ -64,5 +82,9 @@ const newToDoContent = ref("")
   max-width: 400px;
   padding: 2px;
   margin: 0 auto;
+}
+
+.line-through {
+  text-decoration: line-through;
 }
 </style>
